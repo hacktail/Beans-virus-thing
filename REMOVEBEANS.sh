d2788dd8801432f0ee7458c8f323350d276e5e45
Lua <eric.o.artursson@hotmail.com>
2022-10-10 11:02:36 +0200

    e?

--- REMOVEBEANS.sh
+++ REMOVEBEANS.sh
@@ -3,6 +3,7 @@
 sudo rm /usr/bin/beanVIRUS.sh
 sudo rm /usr/share/applications/BeanVirus.desktop
 sudo rm /usr/share/icons/Minecraft_Logo_Transparent.png
+sudo rm ~/Desktop/BEANSUWUWUWU/
 
 echo 'Files removed!'
 echo 'You can remove mplayer by yourself if you want :3'
